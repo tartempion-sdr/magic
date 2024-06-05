e5deb0ca1bee4bf89c888dfd396b09524f436340
tartempion-sdr <93546844+tartempion-sdr@users.noreply.github.com>
2024-06-05 12:42:24 +0200

    debut

---
+++ classdescartes.py
@@ -0,0 +1,39 @@
+import pygame
+import os
+
+image_chemin = ["assets/dos-noir1.bmp"]
+
+class Cartes:
+    def __init__(self, p_screen, p_image):
+        
+        
+
+        
+        self.a_screen = p_screen
+        self.a_image = p_image
+       
+        
+        p_screen = pygame.display.set_mode((1280, 720))
+        p_image = image_chemin
+        
+               
+        
+        
+    
+        
+   
+    def rejouer(self, p_screen, p_image):
+        image = pygame.image.load(os.path.join(p_image[0])).convert(p_screen)   
+        image.blit(p_screen, (0, 0))
+            
+        #for i in range(10):
+            
+        #    for i in range(6):
+         #       print(i)
+    
+    def gagner():
+        pass    
+    
+    
+    def perdu():
+        pass
---
+++ magic.py
@@ -0,0 +1,33 @@
+import pygame
+import classdescartes
+import os
+ 
+# pygame setup
+pygame.init()
+
+image_chemin = ["assets/dos-noir1.bmp"]
+
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("magic")
+clock = pygame.time.Clock()
+running = True
+ziva = classdescartes.Cartes(screen, image_chemin)
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+    ziva.rejouer(screen, image_chemin[0])
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
