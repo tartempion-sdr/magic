2b1bfdd6b031566f79dd443cc1a4a8a01a9b477d
tartempion-sdr <93546844+tartempion-sdr@users.noreply.github.com>
2024-06-08 02:03:02 +0200

    affiche image paire

--- classdescartes.py
+++ classdescartes.py
@@ -5,9 +5,22 @@ import os
 
 image_liste = {}
 screen = pygame.Surface
-image_chemin = "assets/dos-noir1.bmp"
-        
-image = pygame.image.load(os.path.join(image_chemin))
+image_dos_chemin = "assets/dos-noir1.bmp"
+image_chemin = ["assets/rat.bmp", 
+                "assets/poisson.bmp", 
+                "assets/pieuvre.bmp", 
+                "assets/pie.bmp",
+                "assets/lion.bmp", 
+                "assets/fourmis.bmp", 
+                "assets/chien.bmp", 
+                "assets/chat.bmp",
+                "assets/canard.bmp", 
+                "assets/aigle.bmp"]
+
+       
+image = pygame.image.load(os.path.join(image_dos_chemin))
+
+         
 
 decale_x = 0
 decale_y = 0
@@ -23,31 +36,35 @@ class Cartes:
         
         #p_screen = pygame.display.set_mode((1280, 720))
         p_screen = screen
-        p_image = image_chemin
+        p_image = image_dos_chemin
         p_decale_x = decale_x
         p_decale_y = decale_y       
 
-        
+          
         
    
     def rejouer(self, p_screen, p_image, p_decale_x, p_decale_y):
           
         image1 = image.convert(p_screen) 
-            
         for i in range(2):
            
             for e in range(10):
                 
+                random_image = random.choices(image_chemin)
               
-                carte_instance = p_screen.blit(image1, (p_decale_x, p_decale_y))
+                carte_instance = p_screen.blit(image, (p_decale_x, p_decale_y))
                 carte_n0_xy = (p_decale_x, p_decale_y)
                 #creeune liste de tuple coordonee xy
-                image_liste[carte_n0_xy] = [carte_n0_xy, False]
-                
+                image_liste[carte_n0_xy] = [carte_n0_xy, False, image_dos_chemin, random_image]
+                #print(random_image)
+                #
+                #
+                #
+                #image_chemin.remove(random_image)
                 p_decale_x += 50
             p_decale_y += 73
             p_decale_x = 0
-        print(image_liste)
+        print(len(image_liste))
     
     
     def gagner(self):
--- magic.py
+++ magic.py
@@ -5,7 +5,7 @@ import os
 # pygame setup
 pygame.init()
 
-image_chemin = "assets/dos-noir1.bmp"
+image_dos_chemin = "assets/dos-noir1.bmp"
 decale_x = 0
 decale_y = 0
 screen = pygame.display.set_mode((1280, 720))
@@ -15,9 +15,9 @@ running = True
 
 screen.fill("purple")
 
-ziva = classdescartes.Cartes(screen, image_chemin, decale_x, decale_y)
-ziva.rejouer(screen, image_chemin, decale_x, decale_y)
-print(classdescartes.image_liste)
+ziva = classdescartes.Cartes(screen, image_dos_chemin, decale_x, decale_y)
+ziva.rejouer(screen, image_dos_chemin, decale_x, decale_y)
+
 
 
 while running:
@@ -29,9 +29,15 @@ while running:
 
         #action sur une carte
         if event.type == pygame.MOUSEBUTTONDOWN:
-            for i in range(60):
-                image_rect = classdescartes.image.get_rect(topleft=classdescartes.image_liste[i])
+            for i in classdescartes.image_liste:
+                image_rect = classdescartes.image.get_rect(topleft=i)
                 if image_rect.collidepoint(event.pos):
+                    print("clic", classdescartes.image_liste[i][3])
+                    string =  classdescartes.image_liste[i][3]
+                    print(string[0])
+                    image = pygame.image.load(os.path.join(string[0]))
+                    image1 = image.convert(screen) 
+                    carte_instance = screen.blit(image1, (image_rect))
                     print("clic", classdescartes.image_liste[i])
 
     # fill the screen with a color to wipe away anything from last frame
